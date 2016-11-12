aa8187123b982109f16b66c06d6f8970f3a75a16
Eric Butler <neptunecentury@hotmail.com>
2016-11-12 13:15:40 -0500

    HTTP experiments

---
+++ kryptpadapi/src/main/java/com/neptunecentury/kryptpadapi/ApiCredentials.java
@@ -0,0 +1,11 @@
+package com.neptunecentury.kryptpadapi;
+
+/**
+ * Created by neptu on 11/12/2016.
+ */
+
+public class ApiCredentials {
+    public String email;
+    public String password;
+    public String clientId;
+}
--- kryptpadapi/src/main/java/com/neptunecentury/kryptpadapi/KryptPadApi.java
+++ kryptpadapi/src/main/java/com/neptunecentury/kryptpadapi/KryptPadApi.java
@@ -2,15 +2,21 @@ package com.neptunecentury.kryptpadapi;
 
 import android.os.AsyncTask;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 /**
  * Created by Eric Butler on 11/9/2016.
  */
-public abstract class KryptPadApi{
+public abstract class KryptPadApi {
 
-    public static void Authenticate(String email, String password){
-
-    }
+    private static final String HOST = "http://test.kryptpad.com/";
 
     public static class AuthenticateAsync extends AsyncTask<Void, Void, String> {
         /**
@@ -18,19 +24,42 @@ public abstract class KryptPadApi{
          */
         private AsyncTaskComplete _complete;
 
-        public AuthenticateAsync(AsyncTaskComplete complete){
+        public AuthenticateAsync(AsyncTaskComplete complete) {
             _complete = complete;
         }
 
         @Override
-        protected String doInBackground(Void... params){
+        protected String doInBackground(Void... params) {
+
+            try {
+                URL url = new URL(HOST + "api/account/tes3t");
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+                ApiCredentials creds = new ApiCredentials();
+                creds.email = "test@test.com";
+                creds.password = "Abcd!234";
+                creds.clientId = "KryptPadAndroid";
+
+                Gson gson = new Gson();
+                String data = gson.toJson(creds);
+
+                InputStream response = conn.getInputStream();
+
+                if (conn.getResponseCode() == 404) {
+
+                }
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
             return "1234";
         }
 
         @Override
         protected void onPostExecute(final String data) {
             // Call the AsyncTaskComplete class we have stored
-            if (_complete != null){
+            if (_complete != null) {
                 _complete.complete(data, null);
             }
         }
